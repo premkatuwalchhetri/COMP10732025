7385af9fc2f8c595675ea7e75a60540451017e40
premkatuwalchhetri <bahadurkcprem1@gmail.com>
2025-10-19 09:08:43 -0400

    use document.documentElement to target html

---
+++ COMP10732025/lab-2/js/page-color-changer.js
@@ -0,0 +1,9 @@
+//geeting rgb sliders
+var red = document.getElementById("red");
+var green = document.getElementById("green");
+var blue = document.getElementById("blue");
+
+//Select the html element so we can change its styles 
+var body = document.documentElement;
+
+
--- lab-2/js/page-color-changer.js
+++
@@ -1,4 +0,0 @@
-//geeting rgb sliders
-var red = document.getElementById("red");
-var green = document.getElementById("green");
-var blue = document.getElementById("blue");
