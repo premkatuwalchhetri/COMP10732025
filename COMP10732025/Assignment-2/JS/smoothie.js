5238a9f06ab0ac5ead3c211d0235f5e309ecbeeb
premkatuwalchhetri <bahadurkcprem1@gmail.com>
2025-11-05 02:02:23 -0500

    created describition function for Smoothie to show details and added the output area also

--- COMP10732025/Assignment-2/JS/smoothie.js
+++ COMP10732025/Assignment-2/JS/smoothie.js
@@ -1,6 +1,11 @@
+const output = document.getElementById('output');
 // function for the name size ingredients and fruits
 function Smoothie(name, size, ingredients, fruits) {
     this.name = name;
     this.size = size;
     this.ingredients = ingredients;
     this.fruits;
+// this method for the description of the smoothies
+Smoothie.prototype.describe = function() {
+    alert("Smoothie for", this.name,"\nin this Size:", this.size, "\nwhith this ingredients:", this.ingredients, "\nwith special fruith:", this.fruits,);
+};
