7853f0987e840b625210f80a301a7aa75dabc9e7
premkatuwalchhetri <bahadurkcprem1@gmail.com>
2025-11-05 01:46:45 -0500

    created smoothie function for name, size, ingredients and fruits

---
+++ COMP10732025/Assignment-2/JS/smoothie.js
@@ -0,0 +1,6 @@
+// function for the name size ingredients and fruits
+function Smoothie(name, size, ingredients, fruits) {
+    this.name = name;
+    this.size = size;
+    this.ingredients = ingredients;
+    this.fruits;
