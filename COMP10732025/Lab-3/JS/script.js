44fc6643b2ee2da8b167af861ea78085b184e4da
premkatuwalchhetri <bahadurkcprem1@gmail.com>
2025-11-16 01:39:16 -0500

    created methods for the description of the car

--- COMP10732025/Lab-3/JS/script.js
+++ COMP10732025/Lab-3/JS/script.js
@@ -10,5 +10,18 @@ function ToyCar(brand, model, color, scale, price, wheels, material, isElectric,
     this.isElectric = isElectric;
     this.year = year;             
     this.features = features;
-    
+    // this is the method to describe the car
+    this.describe = function() {
+        return "Brand: " + this.brand + "\n" +
+               "Model: " + this.model + "\n" +
+               "Color: " + this.color + "\n" +
+               "Scale: " + this.scale + "\n" +
+               "Price: $" + this.price + "\n" +
+               "Wheels: " + this.wheels + "\n" +
+               "Material: " + this.material + "\n" +
+               "Electric: " + this.isElectric + "\n" +
+               "Year: " + this.year + "\n" +
+               "Features: " + this.features.join(", ");
+    };
 }
+
