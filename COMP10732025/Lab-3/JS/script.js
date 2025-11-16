2886ea5a85256c258718f00aea1bdb9d641928dc
premkatuwalchhetri <bahadurkcprem1@gmail.com>
2025-11-16 01:19:26 -0500

    created constructors for the lab

---
+++ COMP10732025/Lab-3/JS/script.js
@@ -0,0 +1,14 @@
+// created a constructor function for toycars
+function ToyCar(brand, model, color, scale, price, wheels, material, isElectric, year, features) {
+    this.price = price;  
+    this.color = color;           
+    this.scale = scale;                    
+    this.wheels = wheels;         
+    this.material = material; 
+    this.brand = brand;           
+    this.model = model;               
+    this.isElectric = isElectric;
+    this.year = year;             
+    this.features = features;
+    
+}
