4f9d08a349c0e15869df89cbce2b9ff5b18128ce
premkatuwalchhetri <bahadurkcprem1@gmail.com>
2025-12-04 00:05:47 -0500

    created document.querySelector for temperature, description  feels and for humidity to display the weather informaton

--- COMP10732025/Assignment-3/js/weather.js
+++ COMP10732025/Assignment-3/js/weather.js
@@ -2,3 +2,8 @@
 const cityInput = document.querySelector('#city');
 // this selects all the buttons that when clicked this will trigger the weather API request
 const getWeatherBtn = document.querySelector('#getWeather');
+// this selects all the output fields that will display the weather information.
+const tempOutput = document.querySelector('#temp');
+const descOutput = document.querySelector('#desc');
+const feelsOutput = document.querySelector('#feels');
+const humidityOutput = document.querySelector('#humidity');
