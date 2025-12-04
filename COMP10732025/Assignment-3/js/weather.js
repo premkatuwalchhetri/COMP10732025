0c82f5b329fa0c27c705426c54838472140e377a
premkatuwalchhetri <bahadurkcprem1@gmail.com>
2025-12-04 00:12:42 -0500

    created text for temperature display

--- COMP10732025/Assignment-3/js/weather.js
+++ COMP10732025/Assignment-3/js/weather.js
@@ -1,8 +1,8 @@
 // this selects all the text input where the user types the city name
 const cityInput = document.querySelector('#city');
-// this selects all the buttons that when clicked this will trigger the weather API request
+// this selects all the buttons that when clicked this will trigger the weather api request
 const getWeatherBtn = document.querySelector('#getWeather');
-// this selects all the output fields that will display the weather information.
+// this selects all the output fields that will display the weather information
 const tempOutput = document.querySelector('#temp');
 const descOutput = document.querySelector('#desc');
 const feelsOutput = document.querySelector('#feels');
@@ -11,5 +11,6 @@ const humidityOutput = document.querySelector('#humidity');
 const apiKey = "9ba5b4e89060c750bff3447985693325";
 //this is the function for the update weather app
 function updateWeatherInfo(data) {
-
+// this displays the current temperature in Celsius
+    tempOutput.textContent = data.main.temp + " °C";
 }
