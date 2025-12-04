3fc2b0fbcc3b82c981e24bca6612714bb52aabef
premkatuwalchhetri <bahadurkcprem1@gmail.com>
2025-12-03 20:07:06 -0500

    created const weatherbtn so it selects the weather button to add events later

--- COMP10732025/Assignment-3/js/weather.js
+++ COMP10732025/Assignment-3/js/weather.js
@@ -1,2 +1,4 @@
 // this selects all the text input where the user types the city name
 const cityInput = document.querySelector('#city');
+// this selects all the buttons that when clicked this will trigger the weather API request
+const getWeatherBtn = document.querySelector('#getWeather');
